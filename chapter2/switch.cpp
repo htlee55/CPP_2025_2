4e816b37f2c35e8d558403026869555bf685cfae
htlee55 <htlee55@naver.com>
2025-09-09 11:30:12 +0900

    Update switch.cpp

--- chapter2/switch.cpp
+++ chapter2/switch.cpp
@@ -1,1 +1,17 @@
-
+	int number;
+	cout << "숫자를 입력하시오:";
+	cin >> number;
+	switch (number) {
+	case 0:
+		cout << "zero\n";
+		break;
+	case 1:
+		cout << "one\n";
+		break;
+	case 2:
+		cout << "two\n";
+		break;
+	default:
+		cout << "many\n";
+		break;
+	}
