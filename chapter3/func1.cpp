f72de3f33abe6b9ec6b1637df932cbf3007b0c98
htlee55 <htlee55@naver.com>
2025-09-15 16:39:50 +0900

    Update func1.cpp

--- chapter3/func1.cpp
+++ chapter3/func1.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int max(int x, int y)
+{
+  if(x>y)
+    return x;
+  else
+    return y;
+}
+
+int main()
+{
+  int n;
+  n = max(2,3);
+  cout << "연산결과 = " << n << endl;
+  return 0;
+}
