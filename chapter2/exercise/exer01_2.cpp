09303046adcf8f649a7ea27a11d897cc57cd3a05
htlee55 <htlee55@naver.com>
2025-09-16 12:12:13 +0900

    Update exer01_2.cpp

--- chapter2/exercise/exer01_2.cpp
+++ chapter2/exercise/exer01_2.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <cstring>   // strcmp, strcpy, strlen 등
+using namespace std;
+
+int main()
+{
+    char s1[100], s2[100];
+
+    cout << "첫 번째 문자열: ";
+    cin >> s1;
+    cout << "두 번째 문자열: ";
+    cin >> s2;
+
+    if (strcmp(s1, s2) == 0)
+        cout << "2개의 문자열은 같습니다." << endl;
+    else
+        cout << "2개의 문자열은 다릅니다." << endl;
+
+    return 0;
+}
 
