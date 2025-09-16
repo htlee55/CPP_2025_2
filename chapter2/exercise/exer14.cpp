cc2a1cdee909e98936d82a10d092fabbc5d6639d
htlee55 <htlee55@naver.com>
2025-09-16 11:55:56 +0900

    Update exer14.cpp

--- chapter2/exercise/exer14.cpp
+++ chapter2/exercise/exer14.cpp
@@ -1,1 +1,31 @@
-
+# include <iostream>
+using namespace std;
+int main()
+{	char op;
+	double num1, num2;
+	cout << "연산의 종류: ";
+	cin >> op;
+	cout << "숫자를 입력하시오: ";
+	cin >> num1 >> num2;
+	switch (op)
+	{
+	case '+':
+		cout << "계산의 결과 : " <<num1 + num2;
+		break;
+	case '-':
+		cout << "계산의 결과 : "  << num1 - num2;
+		break;
+	case '*':
+		cout << "계산의 결과 : " << num1*num2;
+		break;
+	case '/':
+		if( num2!=0)
+			cout << "계산의 결과 : "  << num1 / num2;
+		break;
+	default:
+		cout << "지원되지 않는 연산자" << endl;
+		break;
+	}
+	cout << endl;
+	return 0;
+}
