a312958f4cf0500c33d7456ccf4f47fbd6248945
htlee55 <htlee55@naver.com>
2025-09-16 11:54:20 +0900

    Update exer06.cpp

--- chapter2/exercise/exer06.cpp
+++ chapter2/exercise/exer06.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+	for (int i = 1; i <= 7; i++) {
+		int j;
+		for (j = 1; j <= i; j++) {
+			cout << j;
+		}
+		for (int k = j; k <= 7; k++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	return 0;
+}
