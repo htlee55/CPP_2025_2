e477d6bca115029788993e6a75d45c98ef68af3b
htlee55 <htlee55@naver.com>
2025-10-13 15:42:31 +0900

    Update lab.cpp

--- chapter5/lab.cpp
+++ chapter5/lab.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+	int width, height;
+public:
+	Rectangle(int w, int h);
+	int calcArea();
+};
+
+Rectangle::Rectangle(int w, int h) 
+{
+	width = w;
+	height = h;
+}
+
+int Rectangle::calcArea() 
+{
+	return width*height;
+}
 
