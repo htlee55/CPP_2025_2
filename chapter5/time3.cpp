91ca7c2ea9d09899704d0856e1c967f62b330ea8
htlee55 <htlee55@naver.com>
2025-10-13 14:14:44 +0900

    Update time3.cpp

--- chapter5/time3.cpp
+++ chapter5/time3.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 class Time {
 public:
 	int hour;		// 0-23
@@ -11,3 +14,17 @@ public:
 		cout << hour << ":" << minute << endl;
 	}
 };
+
+int main()
+{
+	//Time a;
+	Time b(10, 25); 		// ② OK 하지만 예전의 방법이다. 
+	Time c { 10, 25 }; 		// ③ OK 최신의 방법이다. 
+	Time d = { 10, 25 }; 	// ④ OK 하지만 간결하지 않다.
+
+	b.print();
+	c.print();
+	d,print();
+
+	return 0;
+}
