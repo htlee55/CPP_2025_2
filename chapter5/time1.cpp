758e03c1eb403ca80777d4810a0d9ca681f1b189
htlee55 <htlee55@naver.com>
2025-10-13 13:17:16 +0900

    Update time1.cpp

--- chapter5/time1.cpp
+++ chapter5/time1.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+class Time {
+public:
+	int hour;		// 시를 나타낸다. 0-23가 가능하다.
+	int minute;		// 분을 나타낸다. 0-59가 가능하다. 
+
+	void print() {
+		cout << hour << ":" << minute << endl;
+	}
+};
+
+Time a;			// 객체 a를 생성한다. 
+
+a.hour = 26;		
+a.minute = 70;
