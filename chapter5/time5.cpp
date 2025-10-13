81ed3348c13dafdb08e77179afee46cdbef6bd14
htlee55 <htlee55@naver.com>
2025-10-13 18:06:50 +0900

    Update time5.cpp

--- chapter5/time5.cpp
+++ chapter5/time5.cpp
@@ -1,26 +1,27 @@
-
+#include <iostream>
+using namespace std;
 
 class Time {
-private:			// 이후에 선언되는 멤버는 모두 전용 멤버가 된다. 
+public:
 	int hour;		// 0-23
 	int minute;		// 0-59
-public:
-	Time(int h, int m);
-	void inc_hour();
-	void print();
+
+	Time(int h=0, int m=0) {
+		hour = h;
+		minute = m;
+	}
+
+	void print() {
+		cout << hour << ":" << minute << endl;
+	}
 };
 
-Time::Time(int h, int m) {
-	hour = h;
-	minute = m;
-}
+int main()
+{
+	Time a;					// 매개변수가 없는 생성자 호출
+	Time b(10, 25); 		// 매개변수가 있는 생성자 호출
 
-void Time::inc_hour() {
-	++hour;
-	if (hour > 23)
-		hour = 0;
-}
-
-void Time::print() {
-	cout << hour << “:” << minute << endl;
+	a.print();
+	b.print();
+	return 0;
 }
