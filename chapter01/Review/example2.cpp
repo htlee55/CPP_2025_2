8de755e63cfc39017a732fabcbe680c742e240e1
htlee55 <htlee55@naver.com>
2026-03-10 18:00:32 +0900

    Add example2.cpp with character attributes display
    Added a simple C++ program that displays character attributes.

---
+++ chapter01/Review/example2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name = "Knight";
+    int hp = 100;
+    int attack = 20;
+
+    cout << "Name: " << name << endl;
+    cout << "HP: " << hp << endl;
+    cout << "Attack: " << attack << endl;
+
+    return 0;
+}
