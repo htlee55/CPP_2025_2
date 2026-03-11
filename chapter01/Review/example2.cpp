3f8b96fb2f5ea9cb854567ac410a36048cb99f6c
htlee55 <htlee55@naver.com>
2026-03-11 11:52:12 +0900

    Update example2.cpp

--- chapter01/Review/example2.cpp
+++ chapter01/Review/example2.cpp
@@ -10,5 +10,11 @@ int main() {
     cout << "HP: " << hp << endl;
     cout << "Attack: " << attack << endl;
 
+    float pi = 3.14f;       // f 접미사 권장 (없으면 double로 인식됨)
+    float gravity = 9.8F;   // F 대문자도 가능
+    float small = 1.2e-3f;  // 지수 표기법 (0.0012)
+
+    cout << pi << ", " << gravity << std::endl;
+
     return 0;
 }
